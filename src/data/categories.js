b3a5a3e73e8b237c1b07eb3e5e613c0c7f4c1b95
Phuc201205 <pixel321vn@gmail.com>
2026-07-03 17:25:31 +0700

    Home+Components

---
+++ src/data/categories.js
@@ -0,0 +1,25 @@
+import heroImg from "../assets/furniro-hero.png";
+
+export const categories = [
+  {
+    id: 1,
+    name: "Dining",
+    image: heroImg,
+    alt: "Dining room furniture",
+    href: "/shop?category=dining",
+  },
+  {
+    id: 2,
+    name: "Living",
+    image: heroImg,
+    alt: "Living room furniture",
+    href: "/shop?category=living",
+  },
+  {
+    id: 3,
+    name: "Bedroom",
+    image: heroImg,
+    alt: "Bedroom furniture",
+    href: "/shop?category=bedroom",
+  },
+];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import HeroBanner from "../sections/home/HeroBanner.jsx";
+import CategorySection from "../sections/home/CategorySection.jsx";
+import ProductGrid from "../components/ProductGrid.jsx";
+import InspirationSection from "../sections/home/InspirationSection.jsx";
+import InteriorGallery from "../sections/home/InteriorGallery.jsx";
+import { products } from "../data/products.js";
 
-const Home = () => {
+export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <>
+      <HeroBanner />
+      <CategorySection />
+      <ProductGrid title="Our Products" products={products.slice(0, 8)} />
+      <InspirationSection />
+      <InteriorGallery />
+    </>
+  );
 }
-
-export default Home
